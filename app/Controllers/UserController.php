eca909cf4db4c1356d7e0a17d3b0448b793077c3
adriancpp <adrian.cpp@wp.pl>
2023-01-17 20:42:19 +0100

    login register views

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+class UserController extends BaseController
+{
+    public function index()
+    {
+        $data = [];
+
+        helper(['form']);
+
+        echo view('templates/header', $data);
+        echo view('login', $data);
+        echo view('templates/footer', $data);
+    }
+
+    public function register()
+    {
+        $data = [];
+
+        helper(['form']);
+
+        echo view('templates/header', $data);
+        echo view('register', $data);
+        echo view('templates/footer', $data);
+    }
+}
---
+++ app/Views/Templates/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title></title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">Ci4 Login</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/register">Register</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
