e7502a3165d32bfd4930fab7cf9d9641e250f071
adriancpp <adrian.cpp@wp.pl>
2023-01-20 00:40:30 +0100

    tested feature form in form

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controllers;
 
+use App\Models\PrescriptionModel;
+use App\Models\UserModel;
+
 class DashboardController extends BaseController
 {
     public function index()
@@ -19,7 +22,56 @@ class DashboardController extends BaseController
     {
         $data = [];
 
+        helper(['form']);
 
+        if($this->request->getMethod() == 'post' && $this->request->getVar('getDataByPesel') == '1')
+        {
+            return 'dupa';
+
+
+
+        }
+
+
+        if($this->request->getMethod() == 'post')
+        {
+            $rules = [
+//                'recommendation' => 'required',
+//                'lastname' => 'required',
+            ];
+
+            if( !$this->validate($rules))
+            {
+                $data['validation'] = $this->validator;
+            }
+            else {
+                $userEntity = (new UserModel())
+                    ->where('pesel', $this->request->getVar('pesel'))
+                    ->first();
+
+                $patientId = $userEntity->getId();
+
+                //change it later!
+                if($userEntity==null)
+                    $patientId = 0;
+
+                $newPrescription = (new PrescriptionModel());
+
+                $newData = [
+                    'patient_id' => $patientId,
+                    'author_id' => 5,
+                    'recommendation' => '4fdf',
+                    'medicines' => '4fdf',
+                    'security_code' => '4fdf',
+                ];
+
+
+                $newPrescription->save($newData);
+                $session = session();
+                $session->setFlashdata('success', 'Successful Registration');
+                return redirect()->to('/');
+            }
+        }
 
         echo view('templates/header', $data);
         echo view('new_prescription_form', $data);
