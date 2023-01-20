abebf23591ed4a49b1a0142daa0e5e2811f91d88
adriancpp <adrian.cpp@wp.pl>
2023-01-20 16:00:33 +0100

    clear

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,9 +33,9 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => 'qrceptap_root',
-        'password' => 'DGdb889',
-        'database' => 'qrceptap_byt',
+        'username' => '',
+        'password' => '',
+        'database' => '',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
